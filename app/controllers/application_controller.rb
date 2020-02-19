f08279f7de39c75a147446f65f1392fc22ccc3a3
github username <laurenkyu@gmail.com>
2020-02-19 16:30:44 -0500

    Enjoy Alan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,36 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
   end
+
+  get '/articles/new' do
+
+  erb :new
+  end
+
+  post '/articles' do
+    @title = params[:Title]
+    @content = params[:Content]
+    @article = Article.create(:title => @title, :content => @content)
+
+    erb :show
+  end
+
+  get '/articles/:id' do
+    id_number = params[:id]
+    @article = Article.find_by_id(id_number)
+
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+
+    erb :edit
+  end
+
+
 end
