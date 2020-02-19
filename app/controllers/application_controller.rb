4a01aa4133d75f1bc23e2c9d5b0ad7c48803465c
JasonP77 <llvslr77@gmail.com>
2020-02-19 15:37:02 -0500

    second edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
-
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
   end
 
   get '/articles/new' do
@@ -16,6 +16,30 @@ class ApplicationController < Sinatra::Base
     @title = params[:title]
     @content = params[:content]
     Article.create(title: @title, content: @content)
+    erb :show
   end 
 
+  get '/articles' do 
+    @articles = Article.all 
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @id = params[:id]
+    @article = Article.all.find(@id)
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @id = params[:id]
+    erb :edit
+  end
+
+  post '/articles/:id' do
+    @title = params[:title]
+    @content = params[:content]
+    article = Article.find(params[:id])
+    article.update(title: @title, content: @content)
+    
+  end
 end
