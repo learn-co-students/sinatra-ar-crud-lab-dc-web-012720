ac5b9179ee4d9ab67ffcff513ff06fe4c2805126
JasonP77 <llvslr77@gmail.com>
2020-02-19 17:14:10 -0500

    hopefully done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/articles/new' do
+    @article = Article.all
     erb :new
   end
 
@@ -16,7 +17,7 @@ class ApplicationController < Sinatra::Base
     @title = params[:title]
     @content = params[:content]
     Article.create(title: @title, content: @content)
-    erb :show
+    redirect '/articles'
   end 
 
   get '/articles' do 
@@ -43,6 +44,12 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
+  delete '/articles/:id' do
+    @id = params[:id]
+    @articles = Article.all
+    Article.delete(@id)
+    erb :index
+  end
   
 
 end
